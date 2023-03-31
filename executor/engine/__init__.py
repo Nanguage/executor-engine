d08c543094b2b43972034f7bf9924eb208556bff
Nanguage <Nanguage@yahoo.com>
2023-03-31 20:20:39 +0800

    inc version

--- executor/engine/__init__.py
+++ executor/engine/__init__.py
@@ -1,7 +1,7 @@
 from .core import Engine, EngineSetting
 from .job import LocalJob, ThreadJob, ProcessJob
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 __all__ = [
     'Engine', 'EngineSetting',
